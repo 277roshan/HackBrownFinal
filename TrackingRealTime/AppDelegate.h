0dd43f7c24ebaafbad480fcd1ffb8191d8e201fb
Al P <handropando@yahoo.com>
2016-02-06 13:43:45 -0500

    Initial Commit

---
+++ TrackingRealTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TrackingRealTime
+//
+//  Created by AlexsMac on 2/6/16.
+//  Copyright © 2016 HackAtBrown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
